e41bea3b4d401a395837b9fe4dafba2e02456d8e
nuz777 <ivandavidmejiamendez@gmail.com>
2026-08-04 16:42:07 -0500

    feat: agregar editar y eliminar.

--- SaludVital/Program.cs
+++ SaludVital/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SaludVital.UI;
 
-Console.WriteLine("Hello, World!");
+string? opcion;
+do
+{
+    ManagerUser.MostraMenu();
+
+    opcion = Console.ReadLine();
+
+    switch (opcion)
+    {
+        case "1":
+            ManagerMascota.CrearUnaMascota();
+            break;
+        case "2":
+            ManagerMascota.MostrarTodasLasMascotas();
+            break;
+        case "3":
+            ManagerMascota.EditarMascota();
+            break;
+        case "4":
+            ManagerMascota.EliminarMascota();
+            break;
+        case "0":
+            Console.WriteLine("Have a good day!");
+            break;
+        default:
+            Console.WriteLine("te equivocaste de opcion");
+            break;
+    }
+} while (opcion != "0");
